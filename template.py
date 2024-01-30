0fb85bbaf4ea1cc5bb8331b279408d7099039b3d
Towet-Tum <daimacvdl@gmail.com>
2024-01-30 12:48:05 +0300

    packages modified

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
-project_name = "MalariaClassifier"
+project_name = "IntelligentKneeMRIClassifier"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
